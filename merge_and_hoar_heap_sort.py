15d5d1ef6f492a1fb7a5b803235e5e9dffc98cb2
Khusan Turdiev <husanpy@users.noreply.github.com>
2021-01-24 01:55:49 +0500

    Update merge_and_hoar_heap_sort.py

--- merge_and_hoar_heap_sort.py
+++ merge_and_hoar_heap_sort.py
@@ -105,12 +105,6 @@ def hoar_sort(A):
         k += 1
 
 
-print()
-a = [1, 10, 6, 5, 8, 9, 2, 5, 3, 7, 4]
-hoar_sort(a)
-print('inplace hoar_sort a = ', a)
-
-
 def check_sorted(A, ascending=True):
     """this part needs some improvement since
        I want use it like all(list(map(check_sorted, func(arr)))) for func in func_list"""
@@ -126,3 +120,68 @@ def check_sorted(A, ascending=True):
                 flag = False
                 break
     return flag
+
+def inverses_linear(arr):
+    count = 0
+
+    for i in range(len(arr) - 1):
+        for j in range(i + 1, len(arr)):
+            if arr[i] > arr[j]:
+                count += 1
+    return count
+
+
+def merge_inverse(a_list, b_list):
+    """merges two already sorted lists and counts inverses, needs extra memory"""
+    a = a_list[0]
+    b = b_list[0]
+    inverse_count = a_list[1] + b_list[1]
+
+    c = [0] * (len(a) + len(b))
+    i = k = n = 0
+
+    while i < len(a) and k < len(b):
+        if a[i] <= b[k]:
+            c[n] = a[i]
+            i += 1
+            n += 1
+        else:
+            c[n] = b[k]
+            k += 1
+            n += 1
+            inverse_count += len(a) - i
+
+    while i < len(a):
+        c[n] = a[i]
+        i += 1
+        n += 1
+
+    while k < len(b):
+        c[n] = b[k]
+        k += 1
+        n += 1
+
+    return c, inverse_count
+
+
+def inverses_get(a):
+    if len(a) > 1:
+        middle = len(a) // 2
+        l = a[:middle]
+        r = a[middle:]
+        return merge_inverse(inverses_get(l), inverses_get(r))
+    return a, 0
+
+
+def inverses_count(arr):
+    return inverses_get(arr)[1]
+
+
+def main():
+    n = int(input())
+    array = [int(x) for x in input().split()]
+    print(inverses_count(array))
+
+
+if __name__ == '__main__':
+    main()
